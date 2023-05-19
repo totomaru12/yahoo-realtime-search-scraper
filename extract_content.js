e50eea51c3590034344dade3f96f6a2ccfac08c3
t <>
2023-05-19 20:55:59 +0900

    feat: サンプルファイルからの取得するテスト処理を追加

--- extract_content.js
+++ extract_content.js
@@ -1,9 +1,9 @@
 // テスト実行
-const contents = extractRawContents(testSourceString())
-console.log(contents)
+testByCode()
+testByFile()
 
 // Yahooリアルタイム検索結果から結果一覧を取得する
-function extractRawContents (html) {
+function extractContentsByYahooRealtimeSearch (html) {
     const rawContents = extractRawContents(html)
     const contents = removeTags(rawContents)
     return contents
@@ -26,10 +26,29 @@ function removeTags (contents) {
     return result
 }
 
-// テスト用コンテンツ文字列を返す
-function testSourceString () {
+// テスト用コンテンツ文字列を返す (コード定義版)
+function testStringFromCode () {
     let src = '<p class="Tweet_body__XtDoj"><em>テスト</em>、<em>テスト</em></p>'
     src += '<p class="Tweet_body__XtDoj">来月も<em>テストテストテストテスト</em></p>'
     src += '<p class="Tweet_body__XtDoj"><span class="Tweet__reply">返信先:<a href="https://twitter.com/sOcha_yu_" target="_blank" data-cl-params="_cl_link:twlat;_cl_position:9;twid:1659471045227868160;twuid:1476769555783745537">@<!-- -->sOcha_yu_</a></span>そうだ！いつ行く👣？ ｱｰｱｰｯｼｬｺﾗｱｰｱｰｱｰﾃﾒｪｸｿｺﾗｱｰｱｰ<em>ﾃｽﾄﾃｽﾄ</em></p>'
     return src    
+}
+
+// テスト用コンテンツ文字列を返す (ファイル定義版)
+function testStringFromFile () {
+    return require('fs').readFileSync('./sample.html', 'utf8')
+}
+
+// テスト実行: 埋め込み文字列ベース
+function testByCode() {
+    const html = testStringFromCode()
+    const contents = extractContentsByYahooRealtimeSearch(html)
+    console.log(contents)
+}
+
+// テスト実行: サンプルファイルベース
+function testByFile() {
+    const html = testStringFromFile()
+    const contents = extractContentsByYahooRealtimeSearch(html)
+    console.log(contents)
 }
